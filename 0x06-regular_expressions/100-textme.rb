2de023e7d8a3b1bdb4463cfa18f68dbaf225b1bf
Precious Okwuoma Aibueku <aibuekuprecious@gmail.com>
2023-08-01 01:48:49 -0700

    0x06-regular_expressions

---
+++ 0x06-regular_expressions/100-textme.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+#This script outputs: [SENDER],[RECEIVER],[FLAGS]
+
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
---
+++ 0x06-regular_expressions/4-repetition_token_3.rb
@@ -0,0 +1,4 @@
+#!/usr/bin/env ruby
+# This script contains Repetition Token #3
+
+puts ARGV[0].scan(/hbt*n/).join
