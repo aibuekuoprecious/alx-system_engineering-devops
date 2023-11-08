cd73011c4837f685b658fc85b52509d84fdf4295
Aibueku O. Precious <aibuekuprecious@gmail.com>
2023-11-08 15:37:58 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,27 @@
-file { '/var/www/html/wp-includes/class-wp-locale.php':
-  ensure => present,
-  content => '',
+# Install and start Apache
+class { 'apache':
+  docroot => '/var/www/custom',
+}
+
+# Create the custom document root directory
+file { '/var/www/custom':
+  ensure => 'directory',
+  owner  => 'www-data',
+  group  => 'www-data',
+  mode   => '0755',
+}
+
+# Place an index.html file in the custom document root
+file { '/var/www/custom/index.html':
+  ensure  => 'present',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0644',
+  content => 'Hello, world!',
+}
+
+# Set up a virtual host on port 80
+apache::vhost { 'example.com':
+  port    => '80',
+  docroot => '/var/www/custom',
 }
