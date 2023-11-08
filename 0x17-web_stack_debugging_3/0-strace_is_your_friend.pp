2878752a355b0ebf1447cfee704c13519b57c833
Aibueku O. Precious <aibuekuprecious@gmail.com>
2023-11-08 15:08:18 +0100

    Update 0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,4 @@
-# Fix the 500 error by replacing "phpp" with "php" in wp-settings.php
-exec { 'replace':
-  # Use the shell provider to execute the command
-  provider => shell,
-
-  # Replace all occurrences of "phpp" with "php" in wp-settings.php
-  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+file { '/var/www/html/wp-includes/class-wp-locale.php':
+  ensure => present,
+  content => '',
 }
